c0d895ebf5cfc802378019cb22d0dab282183025
dota43ver <facundoagueroc@gmail.com>
2025-01-23 16:40:06 -0300

    agregada la funcionalidad de agregar amigos al array

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+
+function agregarAmigo(){
+    let nombre = document.getElementById("amigo").value;
+    if(nombre.trim().length > 0){
+        amigos.push(nombre);
+        document.getElementById("amigo").value = "";
+        //mostrarAmigos();
+        console.log(amigos);
+    }else{
+        alert("El nombre no puede estar vacío");
+    }
+}
